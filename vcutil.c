ced9543c6f7100d697c3165479b11d824ed9c74c
Steven <steven@ubuntu.(none)>
2014-01-16 01:56:56 -0500

    Huge revision. Implemened new way of getting line ahead by calling line before. However bug: CANT GET RID OF NEWLINE IN STRCAT */

--- vcutil.c
+++ vcutil.c
@@ -58,71 +58,174 @@ VcStatus readVcard( FILE * const vcf, Vcard **const cardp)
 {
     VcStatus newStatus; 
     char * buff;
+    int state = 0; 
     
     if (vcf==NULL)
         newStatus.code = IOERR; 
      /*Checks for begin, and version  */ 
-    while (buff!=NULL)
+    while (state==0)
     {
         newStatus=getUnfolded(vcf,&buff);
         if (newStatus.code == OK)
         {
             /*Send the buff for parsing */
-            printf("Parsing Buffer \n"); 
-            return newStatus;
+            printf("Parsing Buffer %s \n",buff); 
+
         }
         else if (newStatus.code == BEGEND)
             printf("Beggining not found \n");
-            return newStatus;
+        if (buff=='\0')
+        {
+            state =1; 
+            printf("BROKEN\n");
+            break;
+        }
     }
+    return newStatus;
 
 }
 
 VcStatus getUnfolded ( FILE * const vcf, char **const buff )
 {
     /* For each line in the vcard, unfold */
-    /*Its only returning the line that begins with the whitespace.... Why? */ \.
+    /*Its only returning the line that begins with the whitespace.... Why? */ 
     /* Should only assign to the buff once it has been unfolded */ 
     int position; 
-    char lineAhead[30];
+    char lineAhead[200];
+    static char staticLine[200];
+    char  currentLine[200]; 
+    char lineBefore[100];
+
+    static char nextLine[75];
+    int result = 0; 
+    int foldedState;  /* 0 if line is unfolded ok, 1 if needs to be folded */ 
+                char *pos;
+
     VcStatus newStatus; 
-    FILE * copy; 
-    *buff = (char*)calloc(75,sizeof(char));
-    while(fgets(*buff,75,vcf)!=NULL)
-    {
-      position=0;
-      /*Checking to see if the line has a CR */ 
-      position = Contains(*buff,'\r');
-      /*CRLF CHECKING Part1: CR Check */
-      while (position>0)
-      {
-        /*CRLF CHECKING Part 2: LF check */ 
-        if (checkPosition(*buff,position)==1)
+    FILE  * copy; 
+    /* The first time the program runs, ideally we call fgets twice, 
+    for currentLine and for lineAhead
+    
+       I then check to see if the lineAhead is a folded one.
+       If it is, remove spaces, strcat it to the current line, 
+       assign it to buff, and return it. Also I grab the next line and assign
+       it to staticLine. This method is unique so thats why It checks to see
+        that staticLine has a strlen at zero at first.
+
+      In the event that the program is called and staticLine has a strlen greater than zero,
+      a similar thing takes place. The next line is grabbed and checked to see if it is folded, if it is wrap it around, otherwise, 
+      return it as as to buff */
+
+        if (strlen(staticLine)>0)
         {
-            fgets(lineAhead,30,vcf);
-            if (lineAhead[0]==' ')
+           if(fgets(lineAhead,75,vcf)==NULL)
+           {
+             *buff = NULL;
+             return newStatus; 
+           }
+            if (isFolded(lineAhead)==1)
             {
+                printf("wow = %d",removeNewLine(staticLine));
+             //   if ((pos=strchr(staticLine, '\r')) != NULL)
+                //    *pos = '\0';
+                staticLine[strlen(staticLine)-1] = '\0';
+
                 removeSpaces(lineAhead);
-                strncat(*buff,lineAhead,strlen(lineAhead));
-                printf("%s\n",*buff);
-                return newStatus;
+
+                strncat(staticLine,lineAhead,strlen(lineAhead)-1);
             }
         }
-        /*Scans untill a folded line is reached */ 
-        /* If it does, return the folded line */ 
-        /* Otherwise scan the entire file 
-        / * Buffer must contain the files next unfolded line line with trailing EOL removed, Caller is responsible
-        / * To free Buff. VcStatus is   OK and indicates the lines that were unfolded. and line no = 50 and = 100 read. Buff ignored in this case
 
+        else /*First time function is called */ 
+        {
+            if(fgets(staticLine,75,vcf)==NULL)
+            {
+                *buff = NULL;
+                return newStatus; 
+            }
+            if(fgets(lineAhead,75,vcf)==NULL)
+            {
+                *buff = NULL;
+                return newStatus; 
+            }
+            if (isFolded(lineAhead)==1)
+            {
+              printf("wow = %d",removeNewLine(staticLine));
+
+               staticLine[strlen(staticLine)] = '\0';
+                removeSpaces(lineAhead);
+                strncat(staticLine,lineAhead,strlen(lineAhead)-1);
+            }
+        }
+
+        *buff = (char*)calloc(strlen(staticLine),sizeof(char));
+        strcpy(*buff,staticLine);
+        staticLine[0]='\0';
+        strcpy(staticLine,lineAhead);
+        newStatus.code= OK;
+        return newStatus;
+   
+    
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+   
+    if (foldedState==1)
+    {
+        if (strlen(staticLine>0))
+            strcpy(*buff,staticLine); /*This Might Leak 
         
-      }
-        /* Grabbing the next line and appending it to buff 
-        fgets(lineAhead,100,vcf);
-        strncat(*buff,lineAhead,strlen(lineAhead));
-    */ 
+        foldedState=0; /* Buff should now  be unfolded, but the next line also might hav a white space  
+
     }
-    *buff = NULL;
-    return newStatus;
+
+    while (foldedState!=1 || *buff!=NULL)
+    {
+        /* Scanning for unfolded *
+        fgets(*buff,100,vcf);
+        strcpy(staticLine,*buff);
+        copy = vcf; 
+        /* If line is OK, return it 
+        /* how do we know if a line is OK */
+        /* If the next one doesnt start with a whitespace :P 
+        fgets(*buff,75,copy);
+        /* Checking the next line 
+        if (*buff[0]==' ')
+        {
+            printf("This line is a folder! ");
+            foldedState = 1; 
+        }
+    }
+
+    if (foldedState==1)
+    {
+        removeSpaces(*buff);
+        /* Copying buff into a static line, meaning its time to fuck around with *buff and set
+        it back to static line  in a recursive call 
+        strncat(staticLine,buff,sizeof(buff));
+        /*EXIT INDICATING THAT A LINE NEEDS TO BE FOLDED */ 
+        /* Recursive call to do the folding 
+        return VcStatus;
+
+    }
+    
+*/
+
+
 
     
 
@@ -145,6 +248,8 @@ void freeVcFile ( VcFile * const filep)
 int Contains(char * string, char  pattern)
 {
     int i =0;
+    if (string=='\0')
+        return 0;
     for (i=0;i<strlen(string);i++)
     {
         if (string[i]==pattern)
@@ -187,4 +292,26 @@ void removeSpaces(char * string)  /*Function to remove spaces, found from  http:
   *p2 = 0; 
 
 
+}
+
+int isFolded(char * string)
+{
+    if ((string[0]==' ') || (string[0] == '\t'))
+        return 1; 
+    return 0; 
+}
+
+int removeNewLine(char * string)
+{
+    int i = 0; 
+    for (i=0;i<=strlen(string);i++)
+    {
+        if (string[i]=='\n')
+        {
+            string[i]='\0';
+            return 1; 
+        }
+
+    }
+    return 0; 
 }
