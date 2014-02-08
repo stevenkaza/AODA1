c4049fbcc2b76da3a593551c865eeb368a29c84b
Steven Kazavchinski <skazavch@weasley.socs.uoguelph.ca>
2014-02-08 17:39:14 -0500

    Trying to get info to recognize if sortedd

--- vcftool.c
+++ vcftool.c
@@ -43,7 +43,7 @@ int vcfInfo( FILE *const outfile, const VcFile *filep )
 	int photoCounter = 0; 
 	int geoCounter = 0; 
 	int urlCounter = 0; 
-
+	
 	/* if 1, photo has been found in card */ 
 	int photoFound = 0; 
 	int urlFound = 0; 
@@ -108,7 +108,8 @@ int isSorted(VcFile * const filep)
 {
 	int i = 0; 
 	int k = 0;
-
+	char * firstName1; 
+	char * firstName2; 
 	/* Strings to store the value copies of name */ 
 	char * nameValue1=NULL; 
 	char * nameValue2=NULL; 
@@ -155,18 +156,40 @@ int isSorted(VcFile * const filep)
 		
 		nameValue2 = malloc(sizeof(strlen(filep->cardp[i+1]->prop[nCard2].value))); 
 		strcpy(nameValue2,filep->cardp[i]->prop[nCard2].value))); 
+		lastName1 = strtok(nameValue1,";"); 
+		lastName2 = strtok(nameValue2,";"); 
 		result = strcmp(nameValue1,nameValue2); 
 		/* If result == -1, the first string was greater than the second card string,
 		 * indicating that it is sorted. Time to check the next card and increment i*/ 
 		if (result==-1)
 		{
 			continue;
+	
 		}
 		/* if result == 1, the cards are not sorted */ 
 		else if (result==1)
 		{
+			return 0; /* Return 0 indicating they are not sorted*/ 
 
+		}
+		/* The last names happened to be the same between the cards. Time to check first name */ 
+		else
+		{
+			firstName1 = strtok(nameValue1,NULL); 
+			firstName2 = strtok(nameValue2,NULL); 
+			result = strcmp(firstName1,firstName2); 
+			if (result==-1)
+			{
+				continue; 
+			}
+			else if (result==1)
+			{
+				return 0; /* Indicates not sorted */ 
 
+			}
+
+			else 
+			      continue; /*They are equal, continue to the next pair of cards to compare */ 
 		}
 
 		/* Time to compare them */ 
