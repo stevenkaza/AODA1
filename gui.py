8931cbdcd95c26d78a09010501030693d1db3927
Steven Kazavchinski <skazavch@granger.socs.uoguelph.ca>
2014-03-07 16:59:47 -0500

    GUI getting there

--- gui.py
+++ gui.py
@@ -4,28 +4,46 @@
 
 from tkinter import *
 from tkinter import tix 
+from tkinter.scrolledtext import * 
+
 class App:
 	def __init__(self,master): 
-		frame =  Frame(master)
+		frame =  Frame(root)
 		frame.pack() 
+		#menu bar
+		menuBar = Menu(root)
+		#self.master.config(menu=menubar)
+		#menuBar.pack()
+		
+		fileMenu = Menu(menuBar)
+		fileMenu.add_command(label = "exit")
+		menuBar.add_cascade(label='File', menu = fileMenu)
+		self.fvpFrame = Frame(frame) 
+		self.cvpFrame = Frame(frame)
 
-		self.fvp = Button(
-			frame, text="File View Panel", fg="red", command = self.fvp
-			)
-		self.fvp.pack(side=TOP)
+		self.logFrame = Frame(frame)
+		self.logFrame.pack(side = BOTTOM)
 
-		self.cvp = Button(
-			frame,
-			text = "Card View Panel",
-			command = self.cvp)
-		self.cvp.pack(side=BOTTOM) 
+		cvpTixThing = tix.TList(self.cvpFrame,height = 10, width = 40)
+		cvpTixThing.pack()
+
+		fvpTixThing = tix.TList(self.fvpFrame,height = 10, width = 40) 
+		fvpTixThing.pack()
+
+		scrolledLog = ScrolledText(self.logFrame,width = 40,height = 10)
+		scrolledLog.pack() 
+		self.cvpFrame.pack(side=TOP)
+		self.fvpFrame.pack(side =TOP) 
+
+		root.config(menu=menuBar)
+
 	
 	def cvp(self):
 		print ("Card View Panel")
 
 	def fvp(self):
 		print ("File view Panel") 
-root = Tk()
+root = tix.Tk()
 app=App(root)
 
 
