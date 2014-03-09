f44637ebe721627b410402a05f54e8754fbcf537
Steven Kazavchinski <skazavch@longbottom.socs.uoguelph.ca>
2014-03-09 18:22:41 -0400

    invfo

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+import os
 from tkinter import *
 from tkinter import tix
 from tkinter.scrolledtext import *
@@ -11,39 +11,17 @@ class App:
         
         frame.pack()
         #menu bar
-        menuBar = Menu(root)
+        self.menuInit()
         #self.master.config(menu=menubar)
         #menuBar.pack()
 
         #file menu
-        fileMenu = Menu(menuBar)
-        fileMenu.add_command(label = "Open",command = self.fileOpen)
-        fileMenu.add_command(label = "Append")
-        fileMenu.add_command(label = "Save")
-        fileMenu.add_command(label = "Save As")
-        fileMenu.add_command(label = "Exit",command = self.exitProgram)
-
-        #Making it appear
-        menuBar.add_cascade(label='File', menu = fileMenu)
-
+       
         #Organize menu
-        orgMenu = Menu(menuBar)
-        orgMenu.add_command(label = "Sort")
-        orgMenu.add_command(label = "Canonicalize")
-        orgMenu.add_command(label = "Select")
-        orgMenu.add_command(label = "Undo")
+        #self.framesInit()
+        
 
-        menuBar.add_cascade(label='Organize', menu = orgMenu)
-
-        helpMenu = Menu(menuBar)
-
-        helpMenu.add_command(label = "Card flags and colours")
-        helpMenu.add_command(label = "About xvcf")
-        menuBar.add_cascade(label='Help', menu = helpMenu)
-
-
-
-                #Frames
+        #Frames
         self.fvpFrame = Frame(frame)
         self.cvpFrame = Frame(frame)
 
@@ -61,15 +39,17 @@ class App:
         self.cvpFrame.pack(side=TOP)
         self.fvpFrame.pack(side =TOP)
 
-        root.config(menu=menuBar)
 
     def fileOpen(self):
                 ftypes = [('Python files', '*.py'), ('All files', '*')]
                 fname = askopenfilename(filetypes=(("Template files", "*.tplate"),
                                            ("HTML files", "*.html;*.htm"),
                                            ("All files", "*.*") ))
+                self.displayFileInfo(fname)
                #        fl = dlg.show()
 
+    def displayFileInfo(self,fname):
+        os.system("./vcftool -info < %s" % fname)
     def exitProgram(self):
 
         root.destroy()
@@ -78,6 +58,43 @@ class App:
 
     def fvp(self):
         print ("File view Panel")
+        
+        
+    #def framesInit(self):
+
+        
+    def menuInit(self):
+        menuBar = Menu(root)
+
+        menuBar = Menu(root)
+
+        fileMenu = Menu(menuBar)
+        fileMenu.add_command(label = "Open",command = self.fileOpen)
+        fileMenu.add_command(label = "Append")
+        fileMenu.add_command(label = "Save")
+        fileMenu.add_command(label = "Save As")
+        fileMenu.add_command(label = "Exit",command = self.exitProgram)
+
+        #Making it appear
+        menuBar.add_cascade(label='File', menu = fileMenu)
+        
+        orgMenu = Menu(menuBar)
+        orgMenu.add_command(label = "Sort")
+        orgMenu.add_command(label = "Canonicalize")
+        orgMenu.add_command(label = "Select")
+        orgMenu.add_command(label = "Undo")
+
+        menuBar.add_cascade(label='Organize', menu = orgMenu)	
+
+        helpMenu = Menu(menuBar)
+
+        helpMenu.add_command(label = "Card flags and colours")
+        
+        helpMenu.add_command(label = "About xvcf")
+        menuBar.add_cascade(label='Help', menu = helpMenu)
+        root.config(menu=menuBar)
+
+
 root = tix.Tk()
 app=App(root)
 
