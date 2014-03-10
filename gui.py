d8210c034086cdba42295242669a7a08eed2199b
stevenkaza <stevenkazav@gmail.com>
2014-03-09 20:34:12 -0400

    vcfinfo printing into ScrolledText

--- gui.py
+++ gui.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 import os
+import subprocess
 from tkinter import *
 from tkinter import tix
+
 from tkinter.scrolledtext import *
 from tkinter import filedialog
 from tkinter.filedialog import askopenfilename
 class App:
     def __init__(self,master):
         frame =  Frame(root)
-        
+
         frame.pack()
         #menu bar
         self.menuInit()
@@ -16,10 +18,10 @@ class App:
         #menuBar.pack()
 
         #file menu
-       
+
         #Organize menu
         #self.framesInit()
-        
+
 
         #Frames
         self.fvpFrame = Frame(frame)
@@ -34,22 +36,43 @@ class App:
         fvpTixThing = tix.TList(self.fvpFrame,height = 10, width = 60)
         fvpTixThing.pack()
 
-        scrolledLog = ScrolledText(self.logFrame,width = 60,height = 10)
-        scrolledLog.pack()
+        self.scrolledLog = ScrolledText(self.logFrame,width = 60,height = 10)
+        #scrolledLog.insert(END, "This is line %d\n")
+
+        self.scrolledLog.pack()
+
         self.cvpFrame.pack(side=TOP)
         self.fvpFrame.pack(side =TOP)
 
 
     def fileOpen(self):
                 ftypes = [('Python files', '*.py'), ('All files', '*')]
-                fname = askopenfilename(filetypes=(("Template files", "*.tplate"),
-                                           ("HTML files", "*.html;*.htm"),
-                                           ("All files", "*.*") ))
-                self.displayFileInfo(fname)
+                fname = askopenfilename(filetypes=(ftypes))
+                                           
+                                        
+                data= self.displayFileInfo(fname)
+                self.scrolledLog.insert(END, data)
+                                
+                return data
                #        fl = dlg.show()
 
     def displayFileInfo(self,fname):
-        os.system("./vcftool -info < %s" % fname)
+        cmd = ['dir']
+#        print(subprocess.call('dir', shell=True))
+        process = subprocess.Popen(cmd, shell=True,
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE)
+
+       # wait for the process to terminate
+        out, err = process.communicate()
+        errcode = process.returncode
+        #output = subprocess.Popen( cmd, stdout=subprocess.PIPE ).communicate()[0]
+     #print (output)
+        print (out)
+        return out 
+
+
+       # os.system("./vcftool -info < %s" % fname)
     def exitProgram(self):
 
         root.destroy()
@@ -58,11 +81,11 @@ class App:
 
     def fvp(self):
         print ("File view Panel")
-        
-        
+
+
     #def framesInit(self):
 
-        
+
     def menuInit(self):
         menuBar = Menu(root)
 
@@ -77,19 +100,19 @@ class App:
 
         #Making it appear
         menuBar.add_cascade(label='File', menu = fileMenu)
-        
+
         orgMenu = Menu(menuBar)
         orgMenu.add_command(label = "Sort")
         orgMenu.add_command(label = "Canonicalize")
         orgMenu.add_command(label = "Select")
         orgMenu.add_command(label = "Undo")
 
-        menuBar.add_cascade(label='Organize', menu = orgMenu)	
+        menuBar.add_cascade(label='Organize', menu = orgMenu)
 
         helpMenu = Menu(menuBar)
 
         helpMenu.add_command(label = "Card flags and colours")
-        
+
         helpMenu.add_command(label = "About xvcf")
         menuBar.add_cascade(label='Help', menu = helpMenu)
         root.config(menu=menuBar)
@@ -104,3 +127,4 @@ app=App(root)
 #fvp.pack()
 #cvp.pack()
 root.mainloop()
+
